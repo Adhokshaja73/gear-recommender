8b773ab9bb4fdf68da671c4cde5fac621f7057f0
Adhokshaja73 <adhokshaja.acharya@mca.christuniversity.in>
2022-12-13 15:57:21 +0530

    Gears.

--- server/gear_recommender/main/recommender.py
+++ server/gear_recommender/main/recommender.py
@@ -1,17 +1,77 @@
 
-import pandas as pd
+import numpy as np
+import os
+import pickle
+# save the model to disk
 
+# convert the code below into a class
+class GearRecommender:
+    def __init__(self):
+      filePath = os.path.join( os.getcwd(), 'model.sav')
+      self.loaded_model = pickle.load(open(filePath, 'rb'))
 
+    
+    def getGear(self, image, speed, rpm, ):
+      slope = int(self.countPixels(image) > 75)
+      y_pred = self.loaded_model.predict([[speed,rpm, slope]])
+      return(y_pred)
 
+    def countPixels(self, image):
+      avgColorMap = self.getRoadColor()
+      # TODO adjust these parameters based on shape of the image
+      areaMinX = 150
+      areaMaxX = 400
+      areaMinY = 50
+      areaMaxY = 400
+      count = 0
+      pixelCount = 0
+      for i in range(areaMinX, areaMaxX):
+        for j in range(areaMinY,areaMaxY):
+          currentPixel = image[j][i]
+          if(self.colorInRange(currentPixel, avgColorMap)):
+            pixelCount += 1
+          count += 1
+      return (pixelCount/count * 100)
 
-# function that detects if there is slope in the image
-def detectSlope(image):
-    # add the logic here to detect slope
-    return True
+    def colorInRange(self , pixel, avgColorMap):
+      avgColor = avgColorMap["avgColor"]
+      diff = avgColorMap["diff"]
+      r = pixel[0]
+      g = pixel[1]
+      b = pixel[2]
+      if(r > avgColor[0] - diff[0] and r < avgColor[0] + diff[0]):
+          if(g > avgColor[1] - diff[1] and g < avgColor[1] + diff[1]):
+            if(b > avgColor[2] - diff[2] and b < avgColor[2] + diff[2]):
+              return True
+      return False
 
+    def getRoadColor(self, image):
+      roadColor = {"avgColor" : [], "diff" : []}
+      avgColor = [0,0,0]
+      diff = [0,0,0]
+      # TODO adjust these xys based on shape of the input image
+      sampleMinX = 220
+      sampleMaxX = 350
+      sampleY = 350
 
-def recommendGear(image, speed, rpm):
-    gear = 1
-    # add the logic here to recommend gear
+      for i in range(sampleMinX, sampleMaxX):
+        currentPixel = image[sampleY][i]
+        avgColor[0] = (avgColor[0] + currentPixel[0]) // 2
 
-    return gear
+        avgColor[1] = (avgColor[1] + currentPixel[1]) // 2
+
+        avgColor[2] = (avgColor[2] + currentPixel[2]) // 2
+
+      count = 0
+      for i in range(sampleMinX, sampleMaxX):
+        currentPixel = image[sampleY][i]
+        diff[0] = (diff[0] + (currentPixel[0] - avgColor[0])**2)
+        diff[1] = (diff[1] + (currentPixel[1] - avgColor[1])**2)
+        diff[2] = (diff[2] + (currentPixel[2] - avgColor[2])**2)
+        count += 1
+        
+      diff = [int(np.sqrt(i/count)) for i in diff]
+      roadColor["avgColor"] = avgColor
+      roadColor["diff"]  = diff
+      return(roadColor)
+  
--- server/gear_recommender/main/views.py
+++ server/gear_recommender/main/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from .recommender import *
 import cv2
 import numpy as np
 from base64 import b64decode
+from .recommender import *
+
 
 # Create your views here.
 # a function to return homepage
+recommender = GearRecommender()
+
+
 def homePage(request):
     return render(request, 'home.html')
 
@@ -22,9 +26,22 @@ def getGear(request):
     nparr = np.fromstring(data, np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     print(img.shape)
-    # crop the image to get the speedometer from 75% bottom and 80% right of the image
-    # cropped = image[int(image.shape[0]*0.75):, int(image.shape[1]*0.8):]
-    # # use aws rekognition to get the 
-    # recommended_gear = recommendGear(image)
+    
+    speed = getSpeed(img)
+    rpm = getRpm(img)
 
-    return JsonResponse({'gear': 2})
+    recommendedGear = recommender.getGear(img, speed, rpm)
+    return JsonResponse({'gear': recommendedGear})
+
+
+def getSpeed(image):
+    # crop the image where the speed is
+    # get the number in it from aws 
+    # return the number
+    pass
+
+def getRpm(img):
+    # crop the image where the speed is
+    # get the number in it from aws 
+    # return the number
+    pass
