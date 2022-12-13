206c5d69023d71e91773c9767037a6811d1a53a1
Joseph-k-iype <josephkiype@gmail.com>
2022-12-13 14:09:40 +0530

    js final

--- server/gear_recommender/main/urls.py
+++ server/gear_recommender/main/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
+
 urlpatterns = [
     path('', views.homePage),
     path('get_gear', views.getGear),
 ]
 # add media url to urlpatterns
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
